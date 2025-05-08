e7887d5b4a560db1c3cef0f5f29a7a69fad87599
Andrew Kaminer <akaminer@gatech.edu>
2025-05-08 17:27:51 -0400

    Multinomial naive bayes

---
+++ naive_bayes/multinomial/naive_bayes.py
@@ -0,0 +1,38 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class NaiveBayes:
+
+    def fit(self, X, y):
+        self.n_samples, self.n_features = X.shape
+        self.classes = np.unique(y)
+        self.n_classes = self.classes.shape[0]
+        self.feature_probs = np.ones((self.n_classes, self.n_features), dtype=np.float64)
+        self.prior_probs = np.zeros(self.n_classes, dtype=np.float64)
+
+        for idx, cls in enumerate(self.classes):
+            X_class = X[y == cls]
+            word_count = np.sum(X_class)
+            self.prior_probs[idx] = X_class.shape[0] / self.n_samples
+            self.feature_probs[idx] += np.sum(X_class, axis=0)
+            self.feature_probs[idx] /= (word_count + self.n_features)
+
+    def predict(self, x):
+        proportional_probabilities = np.zeros(self.n_classes, dtype=np.float64)
+
+        for i in range(self.n_classes):
+            weighted_feature_probs = (x * self.feature_probs[i])
+            weighted_feature_probs = weighted_feature_probs[weighted_feature_probs > 0]
+            p1 = np.log(self.prior_probs[i])
+            p2 = np.sum(np.log(weighted_feature_probs))
+            prob = p1 + p2
+            proportional_probabilities[i] = prob
+
+        return np.argmax(proportional_probabilities)
+        
+    def batch_predict(self, X):
+        outputs = np.zeros(X.shape[0])
+        for i, x in enumerate(X):
+            outputs[i] = self.predict(x)
+
+        return outputs
---
+++ naive_bayes/multinomial/test_naive_bayes.py
@@ -0,0 +1,45 @@
+import numpy as np
+import pandas as pd
+import sklearn
+import nltk
+from nltk.corpus import stopwords
+
+import string
+import os
+
+from naive_bayes import NaiveBayes
+
+
+def get_data():
+    print("Getting data...")
+    newstrain = sklearn.datasets.fetch_20newsgroups(subset='train')
+    newstest = sklearn.datasets.fetch_20newsgroups(subset='test')
+    nltk.download('stopwords')
+
+    myStopWords = list(string.punctuation) + stopwords.words('english')
+    CV = sklearn.feature_extraction.text.CountVectorizer(max_features=3000,stop_words=myStopWords)
+
+    xtrain = CV.fit_transform(newstrain.data)
+    xtrain = xtrain.toarray()
+    ytrain = newstrain.target
+
+    xtest = CV.fit_transform(newstest.data)
+    xtest = xtest.toarray()
+    ytest = newstest.target
+
+    return xtrain, ytrain, xtest, ytest
+
+def main():
+    xtrain, ytrain, xtest, ytest = get_data()
+    
+    nb = NaiveBayes()
+    nb.fit(xtrain, ytrain)
+    
+    train_res = nb.batch_predict(xtrain)
+    test_res = nb.batch_predict(xtest)
+    print(f"Training accuracy: {np.sum(train_res == ytrain) / ytrain.shape[0]}")
+    print(f"Testing accuracy: {np.sum(test_res == ytest) / ytest.shape[0]}")
+
+
+if __name__ == "__main__":
+    main()
