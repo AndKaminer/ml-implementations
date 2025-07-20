0322f0952ef3b6c7bc3f143eafe0b4e7a51b9046
Andrew Kaminer <akaminer@gatech.edu>
2025-07-19 22:04:06 -0400

    Fix instance path

--- ml_serving/flaskr/__init__.py
+++ ml_serving/flaskr/__init__.py
@@ -6,7 +6,7 @@ from flask import Flask, render_template
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     os.makedirs(app.instance_path, exist_ok=True)
-    app.config.from_file("config.json", load=json.load)
+    app.config.from_file(os.path.join(app.instance_path, "config.json"), load=json.load)
     app.secret_key = "1234"
 
     from .routes import bp as routes_bp
