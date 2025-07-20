998caaaff25ed3de6517b592f7041f970632670f
Andrew Kaminer <akaminer@gatech.edu>
2025-07-19 22:10:55 -0400

    Logging

--- ml_serving/flaskr/__init__.py
+++ ml_serving/flaskr/__init__.py
@@ -6,6 +6,8 @@ from flask import Flask, render_template
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     os.makedirs(app.instance_path, exist_ok=True)
+    print(os.getcwd())
+    print(app.instance_path)
     app.config.from_file(os.path.join(app.instance_path, "config.json"), load=json.load)
     app.secret_key = "1234"
 
