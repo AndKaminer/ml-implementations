4f708d432e8066f75ccdb8efd6a6ad9f66a325d3
Andrew Kaminer <akaminer@gatech.edu>
2025-07-19 22:17:25 -0400

    Fix instance file things

--- ml_serving/flaskr/__init__.py
+++ ml_serving/flaskr/__init__.py
@@ -5,10 +5,14 @@ from flask import Flask, render_template
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
+
     os.makedirs(app.instance_path, exist_ok=True)
-    print(os.getcwd())
-    print(app.instance_path)
-    app.config.from_file(os.path.join(app.instance_path, "config.json"), load=json.load)
+    config_path = os.path.join(app.instance_path, "config.json")
+    if not os.path.exists(config_path):
+        with open(config_path, "w") as f:
+            f.write(json.dumps({}))
+    app.config.from_file("config.json", load=json.load)
+
     app.secret_key = "1234"
 
     from .routes import bp as routes_bp
